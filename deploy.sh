aa1628384cc243b401bc088ef598b2ef43b3cedc
Pelle Bjerkestrand <pelle@pellebjerkstrand.net>
2015-02-28 12:35:50 +0100

    azure build script update

--- deploy.sh
+++ deploy.sh
@@ -24,20 +24,50 @@ if [[ ! -n "$DEPLOYMENT_SOURCE" ]]; then
   DEPLOYMENT_SOURCE=$SCRIPT_DIR
 fi
 
+# Node Helpers
+
+selectNodeVersion () {
+  if [[ -n "$KUDU_SELECT_NODE_VERSION_CMD" ]]; then
+    SELECT_NODE_VERSION="$KUDU_SELECT_NODE_VERSION_CMD \"$DEPLOYMENT_SOURCE\" \"$DEPLOYMENT_TARGET\" \"$DEPLOYMENT_TEMP\""
+    eval $SELECT_NODE_VERSION
+    exitWithMessageOnError "select node version failed"
+
+    if [[ -e "$DEPLOYMENT_TEMP/__nodeVersion.tmp" ]]; then
+      NODE_EXE=`cat "$DEPLOYMENT_TEMP/__nodeVersion.tmp"`
+      exitWithMessageOnError "getting node version failed"
+    fi
+
+    if [[ -e "$DEPLOYMENT_TEMP/.tmp" ]]; then
+      NPM_JS_PATH=`cat "$DEPLOYMENT_TEMP/__npmVersion.tmp"`
+      exitWithMessageOnError "getting npm version failed"
+    fi
+
+    if [[ ! -n "$NODE_EXE" ]]; then
+      NODE_EXE=node
+    fi
+
+    NPM_CMD="\"$NODE_EXE\" \"$NPM_JS_PATH\""
+  else
+    NPM_CMD=npm
+    NODE_EXE=node
+  fi
+}
+
+
 # Build
 echo "Building."
 
 # Install npm packages
 if [ -e "$DEPLOYMENT_SOURCE/package.json" ]; then
     echo "Installing npm packages."
-    eval npm install
+    eval $NPM_CMD install
     exitWithMessageOnError "npm failed"
 fi
 
 # Install and run Gulp
 if [ -e "$DEPLOYMENT_SOURCE/gulpfile.js" ]; then
     echo "Installing Gulp."
-    eval npm install -g gulp
+    eval $NPM_CMD install -g gulp
     exitWithMessageOnError "gulp global install failed"
     echo "Executing Gulp."
     ./node_modules/.bin/gulp
@@ -47,7 +77,7 @@ fi
 # Install and run Grunt
 if [ -e "$DEPLOYMENT_SOURCE/gruntfile.js" ]; then
     echo "Installing Grunt CLI."
-    eval npm install grunt-cli
+    eval $NPM_CMD install grunt-cli
     exitWithMessageOnError "grunt-cli install failed"
     echo "Executing Grunt."
     ./node_modules/.bin/grunt
